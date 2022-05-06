8dee21cb1932eb8dc88a0618aef6f81a82eb372d
TangoctrungTangoctrung <tangoctrung12123@gmail.com>
2022-05-06 11:32:26 +0700

    fix: dev ui header, register

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,21 +1,30 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import * as React from 'react';
+import { MdLogin, MdOutlinePersonAddAlt } from 'react-icons/md';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/#home', label: 'Trang chủ' },
+  { href: '/#service', label: 'Dịch vụ' },
+  { href: '/#about', label: 'Về chúng tôi' },
+  { href: '/#contact', label: 'Liên lạc' },
 ];
 
 export default function Header() {
+  const router = useRouter();
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
+    <header className='sticky top-0 z-50 m-auto flex max-w-[1920px] justify-center border-b-[1.5px] bg-white'>
+      <div className=' flex h-14 w-[1500px] items-center justify-between'>
+        <div className='w-[30%] font-bold hover:text-gray-600'>
+          <UnstyledLink href='/' className='flex items-center'>
+            <Image src='/favicon/logo.png' alt='' height={50} width={50} />
+            <span className='ml-2 text-3xl'>TJob</span>
+          </UnstyledLink>
+        </div>
+        <nav className='w-[40%]'>
+          <ul className='flex items-center justify-around font-semibold'>
             {links.map(({ href, label }) => (
               <li key={`${href}${label}`}>
                 <UnstyledLink href={href} className='hover:text-gray-600'>
@@ -25,6 +34,24 @@ export default function Header() {
             ))}
           </ul>
         </nav>
+
+        <div className='mr-4 flex w-[30%] justify-end text-lg'>
+          <div
+            className='flex cursor-pointer items-center rounded-lg bg-slate-100 px-4 py-2 transition-all duration-300 ease-linear hover:bg-slate-200'
+            onClick={() => router.push('/login')}
+          >
+            <MdLogin />
+            <p className='ml-2 text-base'>Đăng nhập</p>
+          </div>
+          <div className='w-6'></div>
+          <div
+            className='flex cursor-pointer items-center rounded-lg bg-slate-100 px-6 py-2 transition-all duration-300 ease-linear hover:bg-slate-200'
+            onClick={() => router.push('/register')}
+          >
+            <MdOutlinePersonAddAlt />
+            <p className='ml-2 text-base'>Đăng ký</p>
+          </div>
+        </div>
       </div>
     </header>
   );
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,8 @@
+import { Form, Input } from 'antd';
+import Link from 'next/link';
 import * as React from 'react';
 
+import Header from '@/components/layout/Header';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
@@ -9,8 +12,94 @@ export default function RegisterPage() {
       <Seo templateTitle='Register' />
 
       <main>
-        <section className='bg-white'>
-          <h1 className='text-xl text-teal-500'>Register</h1>
+        <Header />
+        <section className='login flex h-[90vh] items-center justify-center bg-white'>
+          <div className='h-[80%] w-[400px] rounded-lg border-[2px] p-6 shadow-lg lg:h-[600px] lg:w-[400px]'>
+            <h1 className='w-full text-center'>
+              <Link href='/'>TJob</Link>
+            </h1>
+            <p className='mt-[16px] w-full text-center text-sm'>
+              Chào mừng bạn, hãy trở thành thành viên của chúng tôi
+            </p>
+
+            <div className='mt-[30px]'></div>
+
+            <Form
+              name='basic'
+              // initialValues={{ remember: true }}
+              autoComplete='off'
+            >
+              <label className='pl-2 font-semibold'>
+                Email<span className='text-red-500'> *</span>
+              </label>
+              <Form.Item
+                name='username'
+                rules={[
+                  { required: true, message: 'Hãy nhập email của bạn!' },
+                  {
+                    pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                    message: 'Email của bạn không đúng định dạng',
+                  },
+                ]}
+              >
+                <Input placeholder='VD: abc@gmail.com' />
+              </Form.Item>
+
+              <div className='mt-[20px]'></div>
+
+              <label className='pl-2 font-semibold '>
+                Mật khẩu<span className='text-red-500'> *</span>
+              </label>
+              <Form.Item
+                name='password'
+                rules={[
+                  { required: true, message: 'Hãy nhập mật khẩu của bạn!' },
+                  {
+                    pattern: new RegExp('(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'),
+                    message:
+                      'Mật khẩu cần có ít nhất 8 kí tự, ít nhất 1 kí tự hoa và 1 kí tự thường',
+                  },
+                ]}
+              >
+                <Input.Password placeholder='VD: Abc!1234' />
+              </Form.Item>
+
+              <div className='mt-[20px]'></div>
+
+              <label className='pl-2 font-semibold '>
+                Nhập lại mật khẩu<span className='text-red-500'> *</span>
+              </label>
+              <Form.Item
+                name='confirmPassword'
+                rules={[
+                  { required: true, message: 'Đây là trường bắt buộc!' },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(
+                        new Error('Mật khẩu nhập lại không đúng!')
+                      );
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password placeholder='VD: Abc!1234' />
+              </Form.Item>
+
+              <div className='mt-[30px]'></div>
+              <div className='flex justify-center'>
+                <button className='w-[250px] rounded-lg bg-black py-4 text-xl font-medium text-white transition-all duration-500 ease-in-out hover:bg-gray-700'>
+                  Đăng ký
+                </button>
+              </div>
+            </Form>
+
+            <p className='m-0 mt-[20px] w-full cursor-pointer text-center text-blue-700 underline hover:text-blue-500'>
+              <Link href='/login'>Bạn đã có tài khoản? Quay lại</Link>
+            </p>
+          </div>
         </section>
       </main>
     </Layout>
